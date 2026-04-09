08a3c89a8695171a4f0d5dda5f60c601da32c056
Aditya Singh <1211821022@ngpp.ac.in>
2026-04-10 01:22:00 +0530

    uncomment the ListNode implementation

--- 2095. Delete the Middle Node of a Linked List/ListNode.java
+++ 2095. Delete the Middle Node of a Linked List/ListNode.java
@@ -1,24 +1,31 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
         ListNode prev = head;
-        while(fast != null && fast.next != null){
+        while (fast != null && fast.next != null) {
             fast = fast.next.next;
             prev = slow;
             slow = slow.next;
         }
-        if(slow == head){
+        if (slow == head) {
             head = null;
             return head;
         }
