557537c61271fab7afe3a04697e2d9e07a313bbc
Aditya Singh <1211821022@ngpp.ac.in>
2026-04-11 01:44:17 +0530

    solved leetcode 61 where i have to rotate LL right by k place

---
+++ 61. Rotate List/Solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null || k == 0)
+            return head;
+        
+        int length = 1;
+        ListNode temp = head;
+
+        while(temp.next != null){
+            length++;
+            temp = temp.next;
+        }
+
+        temp.next = head;
+        k = k % length;
+
+        ListNode newTail = head;
+
+        for(int i=1; i<length-k; i++){
+            newTail = newTail.next;
+        }
+
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+
+        return newHead;
+    }
+}
