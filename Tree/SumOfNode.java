3c7aabfb6fe684fd9119b9ddd7b8f16ae1b07d6d
Aditya Singh <1211821022@ngpp.ac.in>
2026-05-05 01:33:49 +0530

    implement SumOfNode class with methods for building a binary tree and calculating the sum of its nodes

---
+++ Tree/SumOfNode.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class SumOfNode {
+    static class Node {
+        Node left;
+        Node right;
+        int data;
+
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    static int idx = -1;
+
+    static Node buildTree(int[] nodes) {
+        idx++;
+        if (nodes[idx] == -1) {
+            return null;
+        }
+        Node newNode = new Node(nodes[idx]);
+        newNode.left = buildTree(nodes);
+        newNode.right = buildTree(nodes);
+        return newNode;
+    }
+
+    static int sum(Node root) {
+        if (root == null)
+            return 0;
+        int sum = 0;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            Node curNode = q.remove();
+            sum += curNode.data;
+            if (curNode.left != null)
+                q.add(curNode.left);
+            if (curNode.right != null)
+                q.add(curNode.right);
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        Node root = buildTree(nodes);
+        System.out.println(sum(root));
+    }
+}
