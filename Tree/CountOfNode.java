a34bb9b1095d3f5e1bf20bd8fb4bfcc2bb077983
Aditya Singh <1211821022@ngpp.ac.in>
2026-05-05 01:24:25 +0530

    implement CountOfNode class with methods for building a binary tree and counting its nodes

---
+++ Tree/CountOfNode.java
@@ -0,0 +1,40 @@
+public class CountOfNode {
+    static class Node {
+        Node left;
+        Node right;
+        int data;
+
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    static int idx = -1;
+
+    static Node buildTree(int[] nodes) {
+        idx++;
+        if (nodes[idx] == -1) {
+            return null;
+        }
+        Node newNode = new Node(nodes[idx]);
+        newNode.left = buildTree(nodes);
+        newNode.right = buildTree(nodes);
+        return newNode;
+    }
+
+    static int countNode(Node root) {
+        if (root == null)
+            return 0;
+        int lCount = countNode(root.left);
+        int rCount = countNode(root.right);
+        return (lCount + rCount + 1);
+    }
+
+    public static void main(String[] args) {
+        int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        Node root = buildTree(nodes);
+        System.out.println(countNode(root));
+    }
+}
