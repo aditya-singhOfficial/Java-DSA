fabf0889fbd5b24d9a764906c103619c90c6720e
Aditya Singh <1211821022@ngpp.ac.in>
2026-05-05 00:44:45 +0530

    implement TreePreorder class with buildTree method for binary tree construction

---
+++ Tree/TreePreorder.java
@@ -0,0 +1,31 @@
+public class TreePreorder {
+    static class Node {
+        Node left;
+        Node right;
+        int data;
+
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    static int idx = -1;
+
+    static Node buildTree(int[] nodes) {
+        idx++;
+        if (nodes[idx] == -1) {
+            return null;
+        }
+        Node newNode = new Node(nodes[idx]);
+        newNode.left = buildTree(nodes);
+        newNode.right = buildTree(nodes);
+        return newNode;
+    }
+
+    public static void main(String[] args) {
+        int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        System.out.println(buildTree(nodes).data);
+    }
+}
