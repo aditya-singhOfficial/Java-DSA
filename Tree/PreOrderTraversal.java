a6e4fcc231fdeb33eb7deafe04209e0b0e242ca3
Aditya Singh <1211821022@ngpp.ac.in>
2026-05-05 00:48:18 +0530

    implement PreOrderTraversal class with methods for building and traversing a binary tree

---
+++ Tree/PreOrderTraversal.java
@@ -0,0 +1,40 @@
+public class PreOrderTraversal {
+    static class Node {
+        Node left;
+        Node right;
+        int data;
+
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    static int idx = -1;
+
+    static Node buildTree(int[] nodes) {
+        idx++;
+        if (nodes[idx] == -1) {
+            return null;
+        }
+        Node newNode = new Node(nodes[idx]);
+        newNode.left = buildTree(nodes);
+        newNode.right = buildTree(nodes);
+        return newNode;
+    }
+
+    static void PreOrder(Node root) {
+        if (root == null)
+            return;
+        System.out.println(root.data);
+        PreOrder(root.left);
+        PreOrder(root.right);
+    }
+
+    public static void main(String[] args) {
+        int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        Node root = buildTree(nodes);
+        PreOrder(root);
+    }
+}
