a12c1d85d90059cf8cc6c8b2a70bb22e67ff95e8
Aditya Singh <1211821022@ngpp.ac.in>
2026-05-05 02:00:36 +0530

    implement DiameterOfTree class with methods for building a binary tree and calculating its diameter

---
+++ Tree/DiameterOfTree.java
@@ -0,0 +1,55 @@
+public class DiameterOfTree {
+    static class Node {
+        Node left;
+        Node right;
+        int data;
+
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    static int idx = -1;
+
+    static Node buildTree(int[] nodes) {
+        idx++;
+        if (nodes[idx] == -1) {
+            return null;
+        }
+        Node newNode = new Node(nodes[idx]);
+        newNode.left = buildTree(nodes);
+        newNode.right = buildTree(nodes);
+        return newNode;
+    }
+
+    static int findHeight(Node root) {
+        if (root == null)
+            return 0;
+        int leftHeight = findHeight(root.left);
+        int rightHeight = findHeight(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    static int findDiameter(Node root){
+        if(root == null) return 0;
+
+         int leftDiameter = findDiameter(root.left);
+         int leftHeight = findHeight(root.left);
+
+         int rightDiameter = findDiameter(root.right);
+         int rightHeight = findHeight(root.right);
+
+         int self = leftHeight+rightHeight+1;
+
+         return Math.max(rightDiameter, Math.max(leftDiameter, self));
+    }
+
+    public static void main(String[] args) {
+        int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        Node root = buildTree(nodes);
+        System.out.println(findDiameter(root));
+    }
+
+}
