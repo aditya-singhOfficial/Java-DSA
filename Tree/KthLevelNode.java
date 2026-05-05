e57ed21960f1ee352d2b48a456c9a40b480c2608
Aditya Singh <1211821022@ngpp.ac.in>
2026-05-06 00:37:37 +0530

    implement KthLevelNode class with methods for building a binary tree and printing nodes at a specific level

---
+++ Tree/KthLevelNode.java
@@ -0,0 +1,66 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class KthLevelNode {
+    static class Node {
+        Node left;
+        Node right;
+        int data;
+
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    static int idx = -1;
+
+    static Node buildTree(int[] nodes) {
+        idx++;
+        if (nodes[idx] == -1) {
+            return null;
+        }
+        Node newNode = new Node(nodes[idx]);
+        newNode.left = buildTree(nodes);
+        newNode.right = buildTree(nodes);
+        return newNode;
+    }
+
+    static int index = 0;
+
+    static void printKthLevel(Node root, int k) {
+        if (root == null)
+            return;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while (!q.isEmpty()) {
+            Node currNode = q.remove();
+            if (currNode == null) {
+                index++;
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                if (k == index)
+                    System.out.print(currNode.data + " ");
+
+                if (currNode.left != null)
+                    q.add(currNode.left);
+
+                if (currNode.right != null)
+                    q.add(currNode.right);
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, 6, -1, -1, 7, -1, -1 };
+        Node root = buildTree(nodes);
+        printKthLevel(root, 2);
+    }
+}
