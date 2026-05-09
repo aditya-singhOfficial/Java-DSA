d5eb06293c91703f0c4570a3cbf044f1b3cfe522
Aditya Singh <1211821022@ngpp.ac.in>
2026-05-10 02:25:20 +0530

    implement Insertion class with heap functionality for priority queue

--- PriorityQueue/Insertion.java
+++ PriorityQueue/Insertion.java
@@ -1,4 +1,41 @@
 package PriorityQueue;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Insertion {
+    static class Heap {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        void add(int data) {
+            list.add(data);
+
+            int child = list.size() - 1;
+            int parent = (child - 1) / 2;
+
+            while (list.get(child) < list.get(parent)) {
+                int temp = list.get(child);
+                list.set(child, list.get(parent));
+                list.set(parent, temp);
+
+                child = parent;
+                parent = (child - 1) / 2;
+            }
+        }
+
+        void printHeap() {
+            System.out.println(list);
+        }
+    }
+
+    public static void main(String[] args) {
+        Heap h = new Heap();
+        h.add(5);
+        h.add(3);
+        h.add(1);
+        h.add(4);
+        h.add(7);
+        h.add(6);
+        h.printHeap();
+    }
 }
