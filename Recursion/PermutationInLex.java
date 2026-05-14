16d9fa91446d6e5b53036189e577848209092a1d
Aditya Singh <1211821022@ngpp.ac.in>
2026-05-14 23:44:46 +0530

    add: implement PermutationInLex class for generating and sorting all permutations of a given string

---
+++ Recursion/PermutationInLex.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class PermutationInLex {
+
+    static ArrayList<String> allPermutation(char[] s, int pos, ArrayList<String> result) {
+        if (pos == s.length - 1) {
+            result.add(new String(s));
+            return result;
+        }
+
+        for (int i = pos; i < s.length; i++) {
+            swap(s, pos, i);
+            allPermutation(s, pos + 1, result);
+            swap(s, pos, i);
+        }
+        return result;
+    }
+
+    static void swap(char[] s, int si, int ei) {
+        char temp = s[si];
+        s[si] = s[ei];
+        s[ei] = temp;
+    }
+
+    public static void main(String[] args) {
+        String s = "abc";
+        ArrayList<String> result = allPermutation(s.toCharArray(), 0, new ArrayList<>());
+        Collections.sort(result);
+        System.out.println(result);
+    }
+
+}
