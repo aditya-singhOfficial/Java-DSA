ea6e43452a39ab899727b4f7ea035b59af40e563
Aditya Singh <1211821022@ngpp.ac.in>
2026-05-16 02:11:36 +0530

    add: fix formatting and improve output for climbStairs method with DP tabulation

--- DP/Climbing Stairs/Solution.java
+++ DP/Climbing Stairs/Solution.java
@@ -5,9 +5,22 @@ class Solution {
         int[] stairs = new int[n + 1];
         Arrays.fill(stairs, -1);
         System.out.println("Using DP memoization: " + climbUsingDPMemo(n, 0, stairs));
-
+        System.out.println("Using DP Tabuliztion: " + climbUsingDPTab(n));
         return climbUsingRecurssion(n, 0); // using recursion
 
+    }
+
+    static int climbUsingDPTab(int n) {
+        if (n == 1)
+            return 1;
+        int[] stairs = new int[n + 1];
+        stairs[0] = 1;
+        stairs[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            stairs[i] = stairs[i - 1] + stairs[i - 2];
+        }
+
+        return stairs[n];
     }
 
     static int climbUsingDPMemo(int n, int stair, int[] stairs) {
