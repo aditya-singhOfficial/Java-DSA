4de26a1b256c95a22856a79e4158c3b0e5c5214e
Aditya Singh <1211821022@ngpp.ac.in>
2026-05-16 03:49:33 +0530

    add: enhance climbStairs implementation with DP memoization and improve output formatting

--- DP/Climbing Stairs/Solution1.java
+++ DP/Climbing Stairs/Solution1.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Solution1 {
     static int climbStairs(int n) {
         if (n == 0)
@@ -7,7 +9,22 @@ public class Solution1 {
         return climbStairs(n - 1) + climbStairs(n - 2);
     }
 
+    static int climbUsingDPMemo(int n, int[] climbs) {
+        if (n == 0)
+            return 1;
+        if (n < 0)
+            return 0;
+        if (climbs[n] != -1)
+            return climbs[n];
+        climbs[n] = climbStairs(n - 1) + climbStairs(n - 2);
+        return climbs[n];
+    }
+
     public static void main(String[] args) {
-        System.out.println(climbStairs(5));
+        int n = 5;
+        int[] climbs = new int[n + 1];
+        Arrays.fill(climbs, -1);
+        System.out.println("Using Recurssion: " + climbStairs(n));
+        System.out.println("Using DP Memoization: " + climbUsingDPMemo(n, climbs));
     }
 }
