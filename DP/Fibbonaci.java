20aad505866de17010ff757326f67427c98999b9
Aditya Singh <1211821022@ngpp.ac.in>
2026-05-16 01:32:22 +0530

    add: implement fibTabulation method for Fibonacci number calculation using tabulation

--- DP/Fibbonaci.java
+++ DP/Fibbonaci.java
@@ -8,9 +8,19 @@ public class Fibbonaci {
         return f[n];
     }
 
+    static int fibTabulation(int n) {
+        int[] f = new int[n + 1];
+        f[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+        return f[n];
+    }
+
     public static void main(String[] args) {
         int n = 10;
         int[] f = new int[n + 1];
         System.out.println(fib(n, f));
+        System.out.println(fibTabulation(n));
     }
 }
